20d9eac7c7508b87c2c02525628f4915b1b9e1e6
David Santos <insa1988@gmail.com>
2020-02-11 22:12:37 -0500

    primer commit

---
+++ basics/001.hs
@@ -0,0 +1,10 @@
+2 + 2
+--retorna 4
+:t "a"
+--retorna "a" :: [Char]
+[x | x <- [1..5]]
+--list comprehension
+--retorna [1,2,3,4,5]
+(1,2,'a')
+--tupla
+--retorna lo mismo pero es como un objeto que contine 2 int y un char
