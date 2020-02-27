dc82c4c3bb262f8e3ba1ef21f04902889e5035fd
David Santos <insa1988@gmail.com>
2020-02-26 22:56:10 -0500

    operations

---
+++ 002.hs
@@ -0,0 +1,11 @@
+main = do
+    let var1 = 4
+    let var2 = 2
+    putStrLn "La suma es:"
+    print(var1 + var2)
+    putStrLn "La resta es"
+    print(var1 - var2)
+    putStrLn "La division es"
+    print(var1 / var2)
+    putStrLn "La multiplicacion es:"
+    print(var1 * var2)
